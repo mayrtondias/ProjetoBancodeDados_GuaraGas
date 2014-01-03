260e880fd06ed5e9f0c99a7469966caa4bc1c4fb
Mayrton <mayrtondias@hotmail.com>
2014-01-03 20:53:39 -0300

    update index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>
+            GuaraGas
+        </title>
+    </head>
+        
+    
+    <body>
+        <center>
+            <div>
+                <form action="valida.php" method="post">
+                    Login:<br>
+                    <input type="text" name="login"><br>
+                    senha:<br>
+                    <input type="password" name="senha"><br>
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+        </center>
+        <?php
+
+        ?>
+    </body>
+    
+</html>
